353521b2b8279e04fdfc6abea1256d59d44bebd5
ottavio <pedroottavioss@gmail.com>
2024-10-09 21:52:52 -0300

    fornecedores

---
+++ fornecedores/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from .models import Fornecedor
+
+# Create your views here.
+
+class FornecedoresView(ListView):
+    model = Fornecedor
+    template_name = 'fornecedores.html'
+
+    def get_queryset(self):
+        buscar = self.request.GET.get('buscar')
+        qs = super(FornecedoresView, self).get_queryset()
+        if buscar:
+            return qs.filter( nome__icontains=buscar)
+
+        return qs
+
