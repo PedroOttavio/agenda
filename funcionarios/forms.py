8abdad044cf03d084b217ba83d63ddc4d8dd1aed
ottavio <pedroottavioss@gmail.com>
2024-10-22 21:54:54 -0300

    funcionarios btw amostradinho boral birl 2024

---
+++ funcionarios/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+from .models import Funcionario
+
+class FuncionarioModelForm(forms.ModelForm):
+
+    class Meta:
+        model = Funcionario
+        fields = ['nome', 'funcao', 'fone','email','data_admissão', 'foto']
+
+        error_messages = {
+            'nome': {'required': 'O nome do funcionario é um campo obrigatorio'},
+            'funcao': {'required': 'A funcao do funcionario é um campo obrigatorio'},
+            'fone': {'required': 'O fone do funcionario é um campo obrigatorio'},
+            'email': {'required': 'O e-mail do funcionario é um campo obrigatorio',
+                'invalid':'Formato invalido para o e-mail. Exemplo de formato valido: fulano@dominio.com',
+                'unique':'E-mail já cadastrado'
+            },
+            'data_admissao':{'required': ' A data de admissão é um campo obrigatório'},
+        }
+
+
---
+++ funcionarios/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from clientes.models import Pessoa
+
+
+# Create your models here.
+
+class Funcionario(Pessoa):
+    funcao = models.CharField('Função',max_length=35, help_text='Função na empresa')
+    data_admissao = models.DateField('Admissão', help_text='Data de admissão na empresa')
+
+    class Meta:
+        verbose_name = 'Funcionario'
+        verbose_name_plural = 'Funcionarios'
+
+    def __str__(self):
+        return self().nome
