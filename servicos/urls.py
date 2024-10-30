d54335f3df0f615c424287df956ee485e71830ec
ottavio <pedroottavioss@gmail.com>
2024-10-29 21:36:07 -0300

    teste

---
+++ servicos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import ServicosView, ServicosAddView, ServicosUpdateView, ServicosDeleteView
+
+
+urlpatterns = [
+    path('servicos/', ServicosView.as_view(), name='servicos'),
+    path('servico/adicionar', ServicosAddView.as_view(), name='servico-adicionar'),
+    path('<int:pk>/servico/editar', ServicosUpdateView.as_view(), name='servico_editar'),
+    path('<int:pk>/servico/apagar', ServicosDeleteView.as_view(), name='servico_apagar'),
+]
